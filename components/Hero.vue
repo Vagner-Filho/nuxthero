7117770f88444c5470567276f806e6421e6c7a75
Vagner <estagioeducacao6@sesims.com.br>
2021-07-30 12:24:39 -0400

    Starting out

---
+++ components/Hero.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="hero container">
+        <h5>
+            Mutual Funds
+        </h5>
+        <h1>
+            Secure Your <br> Kids Future
+        </h1>
+        <p>
+            Children’s fund is a form of mutual fund with specific child-related goals and terms. Commonly availed investments, solution-oriented plans for the rising cost of education.
+        </p>
+        <button class="button btn btn-know-more">
+            Know More
+        </button>
+        <p>
+            <input type="checkbox" name="" id="30d-free-trial" class="free-trial-check">
+            30 days free trial
+        </p>
+    </section>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    .hero{color: white}
+    .btn-know-more{background-color: pink}
+    .free-trial-check{color: white;}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="kids-future">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-lg-6">
+          <Hero />
+          <NuxtLogo />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style scoped>
+  .kids-future{background: black;}
+</style>
